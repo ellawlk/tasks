b4f0455483320bc0f7e38d688105813ef73ac008
Ella <ellawlk@udel.edu>
2023-02-27 16:01:32 -0500

    Solved makeMath

--- src/arrays.ts
+++ src/arrays.ts
@@ -105,7 +105,16 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    if (addends.length === 0) {
+        return "0=0";
+    } else {
+        const sum = addends.reduce(
+            (total: number, num: number) => total + num,
+            0
+        );
+        const joint = addends.join("+");
+        return sum.toString() + "=" + joint;
+    }
 }
 
 /**
