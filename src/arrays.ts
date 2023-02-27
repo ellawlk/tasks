80f8b01932439e3a24c3d6ae057a66aa5c06041a
Ella <ellawlk@udel.edu>
2023-02-27 15:13:40 -0500

    Solved stringsToIntegers, removeDollars, and maybe bookEndList

--- src/arrays.ts
+++ src/arrays.ts
@@ -47,7 +47,13 @@ export function stringsToIntegers(numbers: string[]): number[] {
  */
 // Remember, you can write functions as lambdas too! They work exactly the same.
 export const removeDollars = (amounts: string[]): number[] => {
-    return [];
+    const remDollars = amounts.map((amt: string): string =>
+        amt.includes("$") ? amt.substring(1, amt.length) : amt
+    );
+    const newList = remDollars.map((num: string): number =>
+        isNaN(+num) ? 0 : +num
+    );
+    return newList;
 };
 
 /**
