6d5c6311aef95bb96d0b0d8cf3d7f7c3626ba9b3
Ella <ellawlk@udel.edu>
2023-03-25 01:20:02 -0400

    "Solved ChangeColor and Multiple ChoiceQuestion. Need to fix edit mode."

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,25 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [currentChoice, setCurrChoice] = useState<string>(options[0]);
+
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setCurrChoice(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="formMCQs">
+                <Form.Label>What is your answer?</Form.Label>
+                <Form.Select value={currentChoice} onChange={updateChoice}>
+                    {options.map((opt: string) => (
+                        // eslint-disable-next-line react/jsx-key
+                        <option value={opt}>{opt}</option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {currentChoice === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
